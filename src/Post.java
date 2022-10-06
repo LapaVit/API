907d75f1432ae15b4a0a9cf58e9e7931ba8c1314
Ivan Lapin <jeti.vano.l@mail.ru>
2022-10-06 18:55:12 +0300

    editing

--- src/Post.java
+++ src/Post.java
@@ -1,10 +1,10 @@
-    public class Post {
-        public String name;
-        public String passport;
-        public String atronymic;
-        public String phone;
-        public String surname;
-        public String subscription;
+public class Post {
+    public String name;
+    public String passport;
+    public String atronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
 
-            public FormDate birthday;
-    }
+    public FormDate birthday;
+}
